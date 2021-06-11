3bf2c10a591617efce9a21be26172f0c13f93f92
SAI PHANI DEEPAK DODDAKA <56880486+Phanideepak@users.noreply.github.com>
2021-06-11 20:47:32 +0530

    Update authenticate.py

--- authenticate.py
+++ authenticate.py
@@ -10,7 +10,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="Alekya@736",
+    passwd="",
     database="mydatabase1",
 auth_plugin="mysql_native_password"
 )
